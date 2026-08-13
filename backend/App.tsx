1467609fbec21e8989173f2e8146589547836231
Laura Cabrejo <laurasofi.cabrejobello@gmail.com>
2026-08-13 05:46:32 -0500

    tengo un error eswtoy solucionando

---
+++ backend/App.tsx
@@ -0,0 +1,36 @@
+// Archivo enrutador
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RegisterScreen } from './src/presentation/views/register/Register';
+import FormulaScreen from './src/screens/FormulaScreen';
+
+// Dentro de tu Stack Navigator
+<Stack.Screen name="Formula" component={FormulaScreen} />
+
+export type RootStackParamList = {
+  RegisterScreen: undefined;
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+// ruta de registro
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterScreen}
+          options={{
+            headerShown: true,
+            title: "Registro",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
