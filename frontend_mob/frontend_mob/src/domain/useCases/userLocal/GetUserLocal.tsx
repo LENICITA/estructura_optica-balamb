7e2cde366cf03f02feefed0604e9983df040410f
LENICITA <149859141+LENICITA@users.noreply.github.com>
2026-08-19 15:50:33 -0500

    Delete frontend_mob/frontend_mob directory

--- frontend_mob/frontend_mob/src/domain/useCases/userLocal/GetUserLocal.tsx
+++
@@ -1,7 +0,0 @@
-import { UserLocalRepositoryImp } from '../../../data/repositories/UserLocalRepository';
-
-const { getUser } = new UserLocalRepositoryImp();
-
-export const GetUserLocalUseCase = async() => {
-  return await getUser();
-}
